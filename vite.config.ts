d534a97abca8fcd327ecaca22dbf48a2602960fc
buzhifanji <buzhifanji@163.com>
2023-01-03 00:41:28 +0800

    配置 base

--- vite.config.ts
+++ vite.config.ts
@@ -71,69 +71,8 @@ export default defineConfig(({ command, mode }) => {
       plugins: [svelte()],
       build: {
         outDir: 'demo',
+        base: 'render-big-data'
       }
     }
   }
 })
-
-
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     svelte({
-//       compilerOptions: {
-//         customElement: process.env.NODE_ENV === 'production' ? true : false// 开发环境 会报错
-//       },
-//       preprocess: sveltePreprocess(),
-//     }),
-//     dts({
-//       outputDir: ['dist/types'],
-//       staticImport: true,
-//       insertTypesEntry: true,
-//       beforeWriteFile: (filePath: string, content: string) => {
-//         if (filePath.includes('index.d.ts')) {
-//           const result = content.replace(/.svelte/g, '')
-
-//           return { filePath, content: result }
-//         }
-//       }
-//     }),
-//   ],
-//   build: {
-//     lib: {
-//       entry: resolve(__dirname, 'src/lib/index.ts'),
-//       name: 'virtual-list',
-//     },
-//     sourcemap: false,
-//     minify: false,
-//     rollupOptions: {
-//       input: ['src/lib/index.ts'],
-//       output: [
-//         {
-//           format: 'es',
-//           //不用打包成.es.js,这里我们想把它打包成.js
-//           entryFileNames: '[name].js',
-//           //让打包目录和我们目录对应
-//           preserveModules: true,
-//           //配置打包根目录
-//           dir: 'dist/es',
-//           preserveModulesRoot: 'src',
-//         },
-//         {
-//           format: 'cjs',
-//           entryFileNames: '[name].cjs',
-//           //让打包目录和我们目录对应
-//           preserveModules: true,
-//           //配置打包根目录
-//           dir: 'dist/cjs',
-//           preserveModulesRoot: 'src'
-//         }
-//       ],
-//       plugins: [
-//         Delete({
-//           targets: ["dist/*.{svg}"]
-//         })
-//       ]
-//     }
-//   },
-// })
